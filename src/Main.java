1fa5bb84a1508918974dd4d4143255465199b1f4
Anderson Filho <filhoando@gmail.com>
2026-03-14 16:31:24 -0300

    feat: printing board

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import chess.ChessMatch;
+import entities.boardgame.Board;
+import entities.boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ChessMatch chessMatch = new ChessMatch();
 
+        UI.printBoard(chessMatch.getPieces());
     }
 }
