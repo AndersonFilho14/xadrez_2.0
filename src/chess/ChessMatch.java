ad9835679781e0667696d895f316874503cc4d5b
Anderson Filho <filhoando@gmail.com>
2026-03-14 17:16:21 -0300

    feat: boad exception

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
+import chess.pieces.King;
+import chess.pieces.Rook;
 import entities.boardgame.Board;
+import entities.boardgame.Position;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -18,5 +22,12 @@ public class ChessMatch {
         }
         return mat;
 
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(3, 2));
+        board.placePiece(new King(board, Color.BLACK), new Position(5, 4));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(5, 3));
+        board.placePiece(new King(board, Color.BLACK), new Position(5, 2));
     }
 }
---
+++ src/entities/boardgame/BoardException.java
@@ -0,0 +1,9 @@
+package entities.boardgame;
+
+public class BoardException extends  RuntimeException{
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+
+}
