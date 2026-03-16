4abde796f7e9039e79d472478d47ef85501219e6
Anderson Filho <filhoando@gmail.com>
2026-03-16 09:58:58 -0300

    moving pieces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import entities.boardgame.Board;
 import entities.boardgame.Position;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,8 +12,21 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+        while (true) {
+
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
