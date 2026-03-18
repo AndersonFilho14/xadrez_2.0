05a59c2157bc8efb54a88c9a5606e1f372aad594
Anderson Filho <filhoando@gmail.com>
2026-03-18 10:34:16 -0300

    feat: pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import entities.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -16,11 +17,22 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
-    public ChessPosition getChessPosition(){
+    public int getMoveCount() {
+        return moveCount;
+    }
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
-    protected boolean isThereOpponentPiece(Position position){
+    protected boolean isThereOpponentPiece(Position position) {
         ChessPiece piece = (ChessPiece) getBoard().piece(position);
         return piece != null && piece.getColor() != color;
     }
